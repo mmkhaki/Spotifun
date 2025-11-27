863981b8586b353d7fd27e518cee2d3f2a4be1a1
Danial Barjasteh siahkalroudy <danial.barjastehsiahkalroudy@email.kntu.ac.ir>
2025-11-27 14:45:06 +0330

    feat: add str function that use when you print these information

--- Music.py
+++ Music.py
@@ -6,3 +6,9 @@ class Music:
         self.year=year
         self.score=score
         self.text=text
+        
+    def __str__(self):
+        return f"Music => name: {self.name} | singer: {self.singer} | id: {self.id} | year: {self.year} | score: {self.score} | text: {self.text}"
+
+    def __repr__(self):
+        return self.__str__()
